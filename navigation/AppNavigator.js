3b3966926808f86f0d14b9dcd65898298dab3cbd
Nish_Munjal <munjalnishant670@gmail.com>
2021-09-10 12:02:12 -0700

    first commit

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,1 @@
+export default Nishant = "Nishant"
---
+++ navigation/NavigationContainer.js
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
+// import { NavigationActions } from 'react-navigation';
+import { NavigationContainer } from "@react-navigation/native";
+
+// import { ProdcutsNavigator } from "./ShopNavigator";
+
+const NavigationConatiner = (props) => {
+  // const navRef = useRef();
+  const isAuth = useSelector((state) => !!state.auth.token);
+
+  // useEffect(() => {
+  //     if (!isAuth) {
+  //         navRef.current.dispatch(
+  //             NavigationActions.navigate({
+  //                 routeName: 'Auth'
+  //             })
+  //         )
+  //     }
+  // }, [isAuth])
+
+  return <NavigationConatiner>
+      <ProdcutsNavigator />
+  </NavigationConatiner>;
+};
+
+export default NavigationConatiner;
