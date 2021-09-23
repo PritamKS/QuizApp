609ffd3b03333a01017d14ee255d0d71b31dde28
Pritam Singh <Pritam.Singh@idc.bedbath.com>
2021-09-23 18:17:25 +0530

    dependency updated

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,4 +1,5 @@
-import { AsyncStorage } from 'react-native';
+// import { AsyncStorage } from 'react-native';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 import Reactotron from 'reactotron-react-native';
 
 Reactotron
